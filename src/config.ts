5cb08bb9f4f9ad63128da8153678d812813a2ceb
Pavel Struhar <pstruhar@gmail.com>
2018-04-13 22:17:58 +0200

    Add more Bible versions

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,31 @@ export const bibles = [
     type: "unbound"
   },
   {
+    id: "beb",
+    lang: "en",
+    name: "Basic English Bible",
+    input: "http://unbound.biola.edu/downloads/bibles/basic_english.zip",
+    pathInArchive: "basic_english_utf8.txt",
+    type: "unbound"
+  },
+  {
+    id: "rves",
+    lang: "es",
+    name: "Reina Valera (1909)",
+    input:
+      "http://unbound.biola.edu/downloads/bibles/spanish_reina_valera_1909.zip",
+    pathInArchive: "spanish_reina_valera_1909_utf8.txt",
+    type: "unbound"
+  },
+  {
+    id: "syno",
+    lang: "ru",
+    name: "Синодальный перевод (1876)",
+    input: "http://unbound.biola.edu/downloads/bibles/russian_synodal_1876.zip",
+    pathInArchive: "russian_synodal_1876_utf8.txt",
+    type: "unbound"
+  },
+  {
     id: "bkr",
     lang: "cz",
     name: "Bible Kralická",
