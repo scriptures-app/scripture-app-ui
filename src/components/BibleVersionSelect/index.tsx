118406cbc8d31ec359b4c13af20d529728447728
Pavel Struhar <pstruhar@gmail.com>
2018-06-22 01:35:44 +0200

    Handle click outside of BibleVersionSelect dropdown

--- src/components/BibleVersionSelect/index.tsx
+++ src/components/BibleVersionSelect/index.tsx
@@ -20,17 +20,57 @@ export default class BibleVersionSelect extends React.Component<
   state: BibleVersionSelectState = {
     open: false
   };
-  handleClick = () => {
+  wrapperRef: HTMLDivElement;
+  buttonRef: HTMLDivElement;
+
+  handleButtonClick = () => {
     this.setState(oldState => ({ ...oldState, open: !oldState.open }));
   };
+
+  handleClose = () => {
+    this.setState(oldState => ({ ...oldState, open: false }));
+  };
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside as (
+      e: {}
+    ) => void);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside as (
+      e: {}
+    ) => void);
+  }
+
+  setWrapperRef = (node: HTMLDivElement) => {
+    this.wrapperRef = node;
+  };
+
+  setButtonRef = (node: HTMLDivElement) => {
+    this.buttonRef = node;
+  };
+
+  handleClickOutside = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (
+      this.wrapperRef &&
+      !this.wrapperRef.contains(event.target as HTMLElement) &&
+      !this.buttonRef.contains(event.target as HTMLElement)
+    ) {
+      // tslint:disable-next-line
+      console.log("ahoj");
+      this.handleClose();
+    }
+  };
+
   render() {
     return (
       <div className="BibleVersionSelect">
-        <div onClick={this.handleClick}>
+        <div onClick={this.handleButtonClick} ref={this.setButtonRef}>
           {this.props.versionId.toUpperCase()}
         </div>
         {this.state.open && (
-          <div className="BibleVersionSelect_dropdown">
+          <div className="BibleVersionSelect_dropdown" ref={this.setWrapperRef}>
             <BibleVersionAutocomplete
               allVersionIds={this.props.allVersionIds}
               versionId={this.props.versionId}
