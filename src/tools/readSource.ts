18a3b3d07e1ef4fcdd237aed3c5c23b626aca392
Pavel Struhar <pstruhar@gmail.com>
2018-11-29 23:59:51 +0100

    Replace fetch with axios in generate script

--- src/tools/readSource.ts
+++ src/tools/readSource.ts
@@ -1,6 +1,6 @@
 import { fs } from "mz";
 import * as AdmZip from "adm-zip";
-import fetch from "node-fetch";
+import axios from "axios";
 
 const extractZip = (buffer: Buffer, pathInArchive: string) => {
   const zip = new AdmZip(buffer);
@@ -20,12 +20,12 @@ export default (sourceFile: string, pathInArchive?: string) => {
   const isZip = sourceFile.substr(-4) === ".zip";
   if (sourceFile.indexOf("http") === 0) {
     const sanitizedUrl = sanitizeUrl(sourceFile);
-    return fetch(sanitizedUrl, { redirect: "follow" }).then(res => {
+    return axios.get(sanitizedUrl, { responseType: "arraybuffer" }).then(res => {
       if (isZip && pathInArchive) {
         // console.log(res.toString());
-        return res.buffer().then(buffer => extractZip(buffer, pathInArchive));
+        return extractZip(res.data, pathInArchive);        
       } else {
-        return res.text();
+        return res.data;
       }
     });
   } else {
