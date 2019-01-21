05937fd6e92fbc0d5d5bd22c8fb991832b0fc150
Pavel Struhar <pstruhar@gmail.com>
2019-01-21 22:49:41 +0100

    Fix types in progressBars

--- src/tools/progressBars.ts
+++ src/tools/progressBars.ts
@@ -1,18 +1,10 @@
 import * as Multiprogress from "multi-progress";
 
+import { BibleInputConfig } from "./interface";
 import { rightPad } from "./rightPad";
 
 const progressbarFormat = ":id - :name (:lang) :bar :percent | :status";
 var multiProgress = new Multiprogress();
-
-interface Bible {
-  id: string;
-  lang: string;
-  name: string;
-  input: string;
-  pathInArchive: string;
-  type: string;
-}
 
 interface Progressbar {
   bar: ProgressBar;
@@ -24,7 +16,7 @@ interface Progressbar {
   };
 }
 
-export function getProgressBars(bibles: Bible[]) {
+export function getProgressBars(bibles: BibleInputConfig[]) {
   const progressbars: Progressbar[] = [];
   for (const { id, lang, name } of bibles) {
     const bar = multiProgress.newBar(progressbarFormat, {
